496d6086219812e362692d64f09c4035c5bef846
Gabriel Kuettel <gabrielkuettel@gmail.com>
2025-03-05 12:50:31 -0700

    chore: format contract

--- examples/starter/smart_contracts/hello_world/contract.algo.ts
+++ examples/starter/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,7 @@
-import { Contract } from "@algorandfoundation/algorand-typescript";
+import { Contract } from '@algorandfoundation/algorand-typescript'
 
 export class HelloWorld extends Contract {
   hello(name: string): string {
-    return `Hello, ${name}`;
+    return `Hello, ${name}`
   }
 }
