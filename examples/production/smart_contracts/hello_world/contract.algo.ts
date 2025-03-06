7bf53a0ffb48ba192792d51a959644b91c53cc4a
Gabriel Kuettel <gabrielkuettel@gmail.com>
2025-03-05 20:21:48 -0700

    feat: use access modifier in non-starter templates; add the eslint rule back

--- examples/production/smart_contracts/hello_world/contract.algo.ts
+++ examples/production/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,7 @@
 import { Contract } from '@algorandfoundation/algorand-typescript'
 
 export class HelloWorld extends Contract {
-  hello(name: string): string {
+  public hello(name: string): string {
     return `Hello, ${name}`
   }
 }
